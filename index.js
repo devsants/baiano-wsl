e6f0d3b7d2a286d7828634723cd8093eec8e44b2
sants <contato.ricardosantana26@gmail.com>
2021-07-22 00:09:16 -0300

    API Criada Baianagem Richarlisson

---
+++ index.js
@@ -0,0 +1,18 @@
+const nodeCmd = require('node-cmd');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/desligar', (req, res) => {
+    res.send('Hello World!');
+    nodeCmd.runSync(`shutdown -s -t ${req.query.timer}`, (err, data, stderr) => console.log(data));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
